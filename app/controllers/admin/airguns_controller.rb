03f018a696274f93299096042e8379ce8df5be35
EC2 Default User <ec2-user@ip-172-31-9-149.ap-northeast-1.compute.internal>
2023-03-25 14:31:09 +0000

    admin index

--- app/controllers/admin/airguns_controller.rb
+++ app/controllers/admin/airguns_controller.rb
@@ -1,3 +1,30 @@
 class Admin::AirgunsController < ApplicationController
 
+  def index
+    @airguns = Airgun.all
+  end
+
+  def show
+    @airgun = User.find(params[:id])
+  end
+
+  def edit
+    @airgun = User.find(params[:id])
+  end
+
+  def update
+    @airgun = Airgun.find(params[:id])
+    if @airgun.update(user_params)
+      flash[:notice] = "You have updated airgun successfully."
+      redirect_to admin_airgun_path(@airgun)
+    else
+      render :edit
+    end
+  end
+
+#   private
+
+#   def user_params
+#     params.require(:user).permit(:email, :name, :is_deleted)
+#   end
 end
